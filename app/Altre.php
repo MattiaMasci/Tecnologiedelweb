b0a544c70d3697af27ec9ecff96c953d5894dd32
Mattia1996 <mascimattia@gmail.com>
2020-02-08 13:33:25 +0100

    Add files via upload

---
+++ app/Altre.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Altre extends Model
+{
+    protected $table = 'altre';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+}
